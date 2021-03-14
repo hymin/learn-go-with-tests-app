08704cb117058b1447ec99f0c9a467eb5e0f650b
Andy Min <andymhy@gmail.com>
2021-03-14 16:30:17 +0800

    using redis as database

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main-server
 
 go 1.13
+
+require github.com/gomodule/redigo v1.8.4 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
 )
+
+var pool *redis.Pool
 
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{map[string]int{}}
@@ -22,8 +28,41 @@ func (i *InMemoryPlayerStore) RecordWin(name string) {
 	i.scores[name]++
 }
 
+func NewRedisPlayerStore(redisKey string) *RedisPlayerStore {
+	return &RedisPlayerStore{redisKey}
+}
+
+type RedisPlayerStore struct {
+	scoreName string
+}
+
+func (r *RedisPlayerStore) GetPlayerScore(name string) (int, bool) {
+	conn := pool.Get()
+	defer conn.Close()
+	score, err := redis.Int(conn.Do("hget", r.scoreName, name))
+	ok := err == nil
+	return score, ok
+}
+
+func (r *RedisPlayerStore) RecordWin(name string) {
+	conn := pool.Get()
+	defer conn.Close()
+	_, err := redis.Int(conn.Do("hincrby", r.scoreName, name, 1))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
-	server := &PlayerServer{NewInMemoryPlayerStore()}
+	pool = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}
+
+	server := &PlayerServer{NewRedisPlayerStore("go_server_score")}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
