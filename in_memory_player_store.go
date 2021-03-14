0a25c3425c577041aaa4facefc88f855f2c5119c
Andy Min <andymhy@gmail.com>
2021-03-14 16:55:11 +0800

    update main.go

--- in_memory_player_store.go
+++ in_memory_player_store.go
@@ -1,1 +1,18 @@
 package main
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+type InMemoryPlayerStore struct {
+	scores map[string]int
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) (int, bool) {
+	score, ok := i.scores[name]
+	return score, ok
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.scores[name]++
+}
--- main.go
+++ main.go
@@ -11,23 +11,6 @@ import (
 
 var pool *redis.Pool
 
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
-
-type InMemoryPlayerStore struct {
-	scores map[string]int
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) (int, bool) {
-	score, ok := i.scores[name]
-	return score, ok
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.scores[name]++
-}
-
 func NewRedisPlayerStore(redisKey string) *RedisPlayerStore {
 	return &RedisPlayerStore{redisKey}
 }
